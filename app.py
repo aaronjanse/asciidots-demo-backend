e39d5a9b12db8a87f14c19c9eaea89ef17b70408
Aaron Janse <gitduino@gmail.com>
2017-02-25 08:53:45 -0800

    re-add try-finally block

--- app.py
+++ app.py
@@ -85,69 +85,70 @@ async def handle_sockets(websocket, path):
 
     stopping = False
 
-    while True:
-        message = await websocket.recv()
+    try:
+        while True:
+            message = await websocket.recv()
 
-        tokens = message.split(';')
+            tokens = message.split(';')
 
-        instruction = tokens[0]
+            instruction = tokens[0]
 
-        if instruction == 'run':
-            if len(tokens) < 2:
-                continue
+            if instruction == 'run':
+                if len(tokens) < 2:
+                    continue
 
-            program = ';'.join(tokens[1:]).split('\n')
+                program = ';'.join(tokens[1:]).split('\n')
 
-            program = [li if len(li) > 0 else ' ' for li in program]
+                program = [li if len(li) > 0 else ' ' for li in program]
 
-            dots_interpreter = None
+                dots_interpreter = None
 
-            try:
-                dots_interpreter = dots_inter_file.DotsInterpreter(program, logging_func=response_func, input_func=input_func)
-            except Exception as e:
+                try:
+                    dots_interpreter = dots_inter_file.DotsInterpreter(program, logging_func=response_func, input_func=input_func)
+                except Exception as e:
+                    await websocket.send('---Starting---\n')
+
+                    await websocket.send('error during preprocessing!\n')
+                    await websocket.send('(stacktrace hidden due to cross origin security reasons)\n')
+                    await websocket.send('>>> try to see if there are any problems with things such as lines starting with \'%\'\n')
+
+                    finished = True
+
+                    continue
+
+                inter_thread = threading.Thread(target=run_interpreter, daemon=True)
+
+                inter_thread.start()
+
                 await websocket.send('---Starting---\n')
+            elif instruction == 'stop' or (finished and pending_txt == ''):
+                if dots_interpreter is not None:
+                    dots_interpreter.terminate()
 
-                await websocket.send('error during preprocessing!\n')
-                await websocket.send('(stacktrace hidden due to cross origin security reasons)\n')
-                await websocket.send('>>> try to see if there are any problems with things such as lines starting with \'%\'\n')
-
-                finished = True
-
-                continue
-
-            inter_thread = threading.Thread(target=run_interpreter, daemon=True)
-
-            inter_thread.start()
-
-            await websocket.send('---Starting---\n')
-        elif instruction == 'stop' or (finished and pending_txt == ''):
-            if dots_interpreter is not None:
-                dots_interpreter.terminate()
-
-                inter_thread.join()
-
-            pending_txt = ''
-
-            await websocket.send('---Stopped---\n\n')
-
-            finished = False
-        elif instruction == 'update':
-            if stopping:
-                stopping = False
-            elif pending_input and not input_done:
-                await websocket.send('input;')
-
-                input_result = await websocket.recv()
-                input_done = True
-            elif pending_txt != '':
-                await websocket.send('out;' + pending_txt)
+                    inter_thread.join()
 
                 pending_txt = ''
-    # except websockets.exceptions.ConnectionClosed as e:
-    #     print('exception caught:')
-    #     print(e)
-    #     dots_interpreter.terminate()
-    #     inter_thread.join()
+
+                await websocket.send('---Stopped---\n\n')
+
+                finished = False
+            elif instruction == 'update':
+                if stopping:
+                    stopping = False
+                elif pending_input and not input_done:
+                    await websocket.send('input;')
+
+                    input_result = await websocket.recv()
+                    input_done = True
+                elif pending_txt != '':
+                    await websocket.send('out;' + pending_txt)
+
+                    pending_txt = ''
+    except websockets.exceptions.ConnectionClosed as e:
+        print('exception caught:')
+        print(e)
+        dots_interpreter.terminate()
+        inter_thread.join()
 
     number_of_sockets -= 1
 
